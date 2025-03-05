123ff8713dd59820d013af7598548a9e18a80a80
Rajabhardwaj127 <2023uce0064@iitjammu.ac.in>
2025-03-05 18:05:21 +0530

    cors address removed

--- Backend_main/index.js
+++ Backend_main/index.js
@@ -45,7 +45,7 @@ console.log(topics)
 
 // Express middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5500', 'http://127.0.0.1:5500'] || "https://project-monitoring.onrender.com",
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5500', 'http://127.0.0.1:5500'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
